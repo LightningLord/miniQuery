8c68b8354a45e8ddc8304298a9b6fab2de50f1a6
Steven G. Harms <steven@devbootcamp.com>
2014-03-18 16:43:06 -0700

    Added DOM module

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,56 @@
 /*!
  * minQuery
  */
+var DOM = {
+  hide: function(selector) {
+          var i,
+            elementSet = this._gatherElementSet(selector);
+
+          for (i = 0; i < elementSet.length; i++) {
+            elementSet[i].style.display = "none";
+          }
+        },
+
+  show: function(selector) {
+          var i,
+            elementSet = this._gatherElementSet(selector);
+
+          for (i = 0; i < elementSet.length; i++) {
+            elementSet[i].style.display = "inherit";
+          }
+        },
+
+  addClass: function(selector, className) {
+              var i,
+                elementSet = this._gatherElementSet(selector);
+
+              for (i = 0; i < elementSet.length; i++) {
+                elementSet[i].className =
+                  elementSet[i].className + " " + className;
+              }
+            },
+
+  removeClass: function(selector, className) {
+              var i,
+                elementSet = this._gatherElementSet(selector);
+
+              for (i = 0; i < elementSet.length; i++) {
+                elementSet[i].className =
+                  elementSet[i].className.replace(className, '').trim();
+              }
+            },
+
+  _gatherElementSet: function(selector) {
+                       var elementsArray;
+
+                       if (selector[0] === ".") {
+                         elementsArray = Array.
+                           prototype.
+                           slice.
+                           call(document.getElementsByClassName("klass"), 0)
+                       } else if (selector[0] === "#") {
+                         elementsArray = Array(document.getElementById('eyed'));
+                       }
+                       return elementsArray;
+                     }
+}
