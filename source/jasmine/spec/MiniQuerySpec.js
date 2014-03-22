2b15a4183a3bc30a77c03bf73e9c2c9a0ad3a5e7
Steven G. Harms <steven@devbootcamp.com>
2014-03-22 06:56:42 -0700

    Preliminary MiniQuery test

--- source/jasmine/spec/MiniQuerySpec.js
+++ source/jasmine/spec/MiniQuerySpec.js
@@ -1,5 +1,26 @@
-describe("Hello world", function() {
-    it("says hello", function() {
-        expect(helloWorld()).toEqual("Hello world!");
+describe("MiniQuery module", function() {
+    it("is defined", function() {
+        expect($).toBeDefined();
+    });
+
+    it("finds an element by class", function() {
+      var result = $(".klass");
+
+      expect(result.slice).toBeDefined();
+      expect(result.length).toBe(1);
+    });
+
+    it("finds an element by id", function() {
+      var result = $("#eyed");
+
+      expect(result.slice).toBeDefined();
+      expect(result.length).toBe(1);
+    });
+
+    it("finds an element by tag and class", function() {
+      var result = $("div.klass");
+
+      expect(result.slice).toBeDefined();
+      expect(result.length).toBe(1);
     });
 });
