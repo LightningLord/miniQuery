ab5adeaa47ff68796b9f7f5cae9f7c6227a6434e
Matt Bechtel & Ryan <pair+mbech.rbn1357@devbootcamp.com>
2014-04-08 12:03:30 -0700

    Add removeClass function to DOM module
    * Refactored: changed div_elems to divElems
    * Did more stuff

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -9,16 +9,22 @@ var SweetSelector = (function(){
 var DOM = (function(){
   return {
     hide: function(selector){
-      //style="dispaly: none;"
-      var div_elems = SweetSelector.select(selector)
-      for(var i = 0; i < div_elems.length; i++){
-       div_elems[i].style.display = 'none';
+      var divElems = SweetSelector.select(selector)
+      for(var i = 0; i < divElems.length; i++){
+       divElems[i].style.display = 'none';
       }
     },
     show: function(selector){
-      var div_elems = SweetSelector.select(selector)
-      for(var i = 0; i < div_elems.length; i++){
-       div_elems[i].style.display = '';
+      var divElems = SweetSelector.select(selector)
+      for(var i = 0; i < divElems.length; i++){
+       divElems[i].style.display = '';
+      }
+    },
+    removeClass: function(selector, className){
+      var divElems = SweetSelector.select(selector)
+      for(var i = 0; i < divElems.length; i++){
+       divElems[i].classList.remove(className)
+       console.log(divElems[i])
       }
     }
   }
