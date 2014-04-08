d390872cd1b2a4a41743f742a57ad1b74eba7570
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-04-08 11:30:03 -0700

    release 1 done
    hide, show, add, and remove classes from dom elements

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -2,6 +2,10 @@
  * minQuery
  */
 
+window.onload = function() {
+    dino = new DOM;
+};
+
 function SweetSelector(selector) {
     if (selector[0] == "#") {
         var toReturn = document.getElementById(selector.substr(1, selector.length - 1));
@@ -10,5 +14,30 @@ function SweetSelector(selector) {
     } else {
         var toReturn = document.getElementsByTagName(selector);
     }
-    return toReturn;
+    if (typeof toReturn == "object") {
+        return toReturn[0];
+    } else {
+        return toReturn;
+    }
+};
+
+
+function DOM() {
+    this.hide = function(element) {
+        SweetSelector(element).style.visibility = "hidden";
+    },
+
+    this.show = function(element) {
+        SweetSelector(element).style.visibility = "visible";
+    },
+
+    this.addKlass = function(element, klassToAdd) {
+        SweetSelector(element).className += klassToAdd
+    }
+
+    this.removeKlass = function(element, klassToRemove) {
+        element = SweetSelector(element)
+        element.className = element.className.replace(klassToRemove, '');
+    }
+
 }
