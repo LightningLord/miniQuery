1dc968e8120ec813db71b554615a83b864859806
Katherine Hayden & Jason Lo <pair+katherineimogene.lightninglord@devbootcamp.com>
2014-04-08 10:52:52 -0700

    Implement select function
      *based on first character in selector
        *can select by id (#)
        *can select by class (.)
        *can select by tag

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,21 @@
 /*!
  * minQuery
  */
+var SweetSelector = function(){
+
+}
+
+SweetSelector.prototype = {
+  select: function(selector){
+    var selectorName = selector.substring(1)
+    if (selector[0] == '#'){
+      return document.getElementById(selectorName)
+    } else if (selector[0] == '.'){
+      return document.getElementsByClassName(selectorName)
+    } else {
+      return document.getElementsByTagName(selector)
+    }
+  }
+}
+
+var sweetSelector = new SweetSelector
