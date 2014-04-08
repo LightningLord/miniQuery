61efa6c33c02f92a06d83f34b8852c8078c4498f
Mason O'Neal <masonwid@gmail.com>
2014-04-08 14:29:00 -0700

    Add initial selector function code

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,38 @@
 /*!
  * minQuery
  */
+
+function SweetSelector(element) {
+  debugger
+  var firstChar = element.charAt(0);
+
+  switch (firstChar) {
+  case '.':
+    console.log("it's a class");
+    var elementObject = document.getElementsByClassName(element)
+    return elementObject
+  case '#':
+    console.log("it's an ID");
+    var elementObject = document.getElementById(element);
+    return elementObject
+  case 'a':
+
+  }
+
+  console.log(elementObject)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*!
+ SweetSelector.select('#eyed')
+ */
