3841583fe0fa4702fc4e01322aee10cb9692ad55
Germain Boue & Janet Yi <pair+boue.krnwonseungee@devbootcamp.com>
2014-04-08 11:03:24 -0700

    Start some pseudocode along with some details
      *Frame for the model
      *Need to find a way to select elements

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,45 @@
 /*!
  * minQuery
  */
+// PSEUDOCODE
+// FIND ELEMENT BY ID: var x=document.getElementById("intro");
+// FIND ELEMENT BY CLASS: document.getElementsByClassName("intro");
+// FIND ELEMENT BY TAG NAME: var y=x.getElementsByTagName("p");
+// <p id="intro">Hello World!</p>
+// <p>This example demonstrates the <b>getElementById</b> method!</p>
+
+// <script>
+// x=document.getElementById("intro");
+// document.write("<p>The text from the intro paragraph: " + x.innerHTML + "</p>");
+// </script>
+// document.getElementById('contents'); //returns a HTML DOM Object
+
+
+// var txt = "";
+// for (var i=0;i<x.length;i++)
+//   {
+//   txt = txt + x.elements[i].value + "<br>";
+//   }
+
+
+  // var eyed = document.querySelectorAll('div #eyed');
+  var eyed = document.getElementById("eyed")
+  var klass = document.getElementsByClassName("klass");
+  var hello = document.getElementsByTagName("a"); //the link
+
+
+var SweetSelector = (function () {
+
+  return {
+    select: function(element) {
+      // So if it starts with #, . , or tag different rules
+    };
+
+
+  };
+
+})
+
+SweetSelector.select("#eyed");
+SweetSelector.select(".klass");
+SweetSelector.select("a");
