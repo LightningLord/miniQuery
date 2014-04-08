3e9d323da5af5b54e4495ddb32bb9bdcd16b6720
Katherine Hayden & Jason Lo <pair+katherineimogene.lightninglord@devbootcamp.com>
2014-04-08 11:13:21 -0700

    Implemented show and hide
      * select the elements
      * walk through and set style.display

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,11 +1,11 @@
 /*!
  * minQuery
  */
-var SweetSelector = function(){
+ var SweetSelector = function(){
 
-}
+ }
 
-SweetSelector.prototype = {
+ SweetSelector.prototype = {
   select: function(selector){
     var selectorName = selector.substring(1)
     if (selector[0] == '#'){
@@ -16,6 +16,33 @@ SweetSelector.prototype = {
       return document.getElementsByTagName(selector)
     }
   }
+
+
 }
 
+
+
 var sweetSelector = new SweetSelector
+
+
+var DOM = function(){
+
+}
+
+DOM.prototype = {
+  hide: function(selector){
+    var domCollection = sweetSelector.select(selector)
+    for (var i = 0; i < domCollection.length; i++){
+      domCollection[i].style.display = 'none'
+    }
+  },
+  show: function(selector){
+    var domCollection = sweetSelector.select(selector)
+    for (var i = 0; i < domCollection.length; i++){
+      domCollection[i].style.display = 'block'
+    }
+  }
+
+}
+
+var dom = new DOM
