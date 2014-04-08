e0b6856a5cd18c534a8e4e48e58b637935849a86
Drew Parker <apprentice@dbc32.local>
2014-04-08 11:40:20 -0700

    adds ability to select html element with SweetSelector Module

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,48 @@
 /*!
  * minQuery
  */
+
+ window.addEventListener("load", function load(event){
+  window.removeEventListener("load", load, false);
+  console.log(SweetSelector.select("#eyed"));
+  console.log(SweetSelector.select(".klass"));
+  console.log(SweetSelector.select("a"));
+ },false)
+
+
+  var SweetSelector = {
+
+  select: function(passed) {
+    if (passed[0] == '#') {
+      var id = passed.slice(1,passed.length)
+      return document.getElementById(id)
+  } else if (passed[0] == '.') {
+      var className = passed.slice(1,passed.length)
+      return document.getElementsByClassName(className)[0]
+  } else {
+      var tag = passed
+      return document.getElementsByTagName(tag)[0]
+  }
+
+  }
+}
+
+
+
+
+
+// function miniQuery(){
+//   alert("here")
+
+
+
+//   SweetSelector.select("stuff")
+// }
+
+
+  // console.log('success')
+  // console.log('x')
+
+  // // var x = document.getElementById("#eyed")
+  // // console.log("x")
+  // )
