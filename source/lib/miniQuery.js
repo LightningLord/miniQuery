a697039537ba8296720db4c8aa71578525083d32
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-04-08 12:31:13 -0700

    ajax kind of working

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -2,42 +2,60 @@
  * minQuery
  */
 
-window.onload = function() {
-    dino = new DOM;
-};
-
-function SweetSelector(selector) {
-    if (selector[0] == "#") {
-        var toReturn = document.getElementById(selector.substr(1, selector.length - 1));
-    } else if (selector[0] == ".") {
-        var toReturn = document.getElementsByClassName(selector.substr(1, selector.length - 1));
-    } else {
-        var toReturn = document.getElementsByTagName(selector);
-    }
-    if (typeof toReturn == "object") {
-        return toReturn[0];
-    } else {
-        return toReturn;
+var SweetSelector = {
+    select: function(selector) {
+        if (selector[0] == "#") {
+            var toReturn = document.getElementById(selector.substr(1, selector.length - 1));
+        } else if (selector[0] == ".") {
+            var toReturn = document.getElementsByClassName(selector.substr(1, selector.length - 1));
+        } else {
+            var toReturn = document.getElementsByTagName(selector);
+        }
+        if (typeof toReturn == "object") {
+            return toReturn[0];
+        } else {
+            return toReturn;
+        }
     }
 };
 
 
-function DOM() {
-    this.hide = function(element) {
+var DOM = {
+    hide: function(element) {
         SweetSelector(element).style.visibility = "hidden";
     },
-
-    this.show = function(element) {
+    show: function(element) {
         SweetSelector(element).style.visibility = "visible";
     },
-
-    this.addKlass = function(element, klassToAdd) {
+    addKlass: function(element, klassToAdd) {
         SweetSelector(element).className += klassToAdd
-    }
-
-    this.removeKlass = function(element, klassToRemove) {
+    },
+    removeKlass: function(element, klassToRemove) {
         element = SweetSelector(element)
         element.className = element.className.replace(klassToRemove, '');
     }
+};
 
+// var EventDispatcher = {
+//     // on: function(selector, functionName, function) {
+//     // var functionName = Event(functionName);
+//     // console.log(functionName);
+//     on: function(selector, newName, boundFunction) {
+//         var event = new Event(newName);
+//         var hi = SweetSelector.select(selector);
+//         console.log(hi);
+//         hi.addEventListener(newName, boundFunction)
+//         console.log(event);
+//     }
+// }
+
+var AjaxWrapper = {
+    request: function(objsWithParams) {
+        var oReq = new XMLHttpRequest();
+        oReq.open(objsWithParams.type, objsWithParams.url, true);
+        oReq.send();
+        console.log(oReq);
+        console.log(oReq.responseXML);
+        var hi = oReq;
+    }
 }
