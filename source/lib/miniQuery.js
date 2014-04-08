70c1c7ffe1e27b166646c7b1caa3897b9b60f998
Matt Bechtel & Ryan <pair+mbech.rbn1357@devbootcamp.com>
2014-04-08 11:44:01 -0700

    DOM manipulation part 1:
    *hide()
    *show()

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,8 +1,25 @@
 var SweetSelector = (function(){
-
   return {
     select: function(selector) {
       return document.querySelectorAll(selector)
+    }
+  }
+})();
+
+var DOM = (function(){
+  return {
+    hide: function(selector){
+      //style="dispaly: none;"
+      var div_elems = SweetSelector.select(selector)
+      for(var i = 0; i < div_elems.length; i++){
+       div_elems[i].style.display = 'none';
+      }
+    },
+    show: function(selector){
+      var div_elems = SweetSelector.select(selector)
+      for(var i = 0; i < div_elems.length; i++){
+       div_elems[i].style.display = '';
+      }
     }
   }
 })();
