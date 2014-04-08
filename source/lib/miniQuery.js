75e83db94f23dda591ecd762a8f3e33196adbac1
Matt Bechtel & Ryan <pair+mbech.rbn1357@devbootcamp.com>
2014-04-08 14:51:42 -0700

    Implemented ajax get request functionality.
    * Can't figure out way to actually test as it won't let us make cross-site requests...
    * But it works on success when requesting the current local file path.
    * -Ryan

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -34,8 +34,60 @@ var DOM = (function(){
       var divElems = SweetSelector.select(selector)
       for(var i = 0; i < divElems.length; i++){
        divElems[i].classList.add(className)
-       console.log(divElems[i])
       }
     }
   }
 })();
+
+var EventDispatcher = (function() {
+  var events = {}
+
+  return {
+    on: function(selector, eventName, eventHandler){
+     var divElems = SweetSelector.select(selector)
+     for(var i = 0; i < divElems.length; i++){
+       divElems[i].addEventListener(eventName, eventHandler)
+      }
+    },
+
+    trigger: function(selector, eventName){
+      var event = new Event(eventName)
+      var divElems = SweetSelector.select(selector)
+      for(var i = 0; i < divElems.length; i++){
+        divElems[i].dispatchEvent(event)
+      }
+    }
+  }
+})();
+
+var AjaxWrapper = (function(){
+  return {
+    request: function(reqParams){
+      request = new XMLHttpRequest()
+      request.open(reqParams.type, reqParams.url, true)
+
+      request.onload = function(){
+        if (request.status >= 200 && request.status < 400) {
+          resp = request.responseText;
+          reqParams.success(resp)
+        }
+        else {
+          console.log(reqParams)
+          reqParams.fail();
+        }
+      }
+      request.send()
+    }
+  }
+})();
+
+// {
+//  url: 'someurl',
+//  type: 'GET',
+//  success: function() {
+//    //do something
+//  },
+//  fail: function() {
+//   //do something
+//  }
+// }
