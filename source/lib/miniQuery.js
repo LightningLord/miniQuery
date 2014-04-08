0b35be6b2f607892039595d280fd579e7eeb4551
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-04-08 10:41:11 -0700

    SweetSelector working
    * works with ids, classes, and normal tags

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,14 @@
 /*!
  * minQuery
  */
+
+function SweetSelector(selector) {
+    if (selector[0] == "#") {
+        var toReturn = document.getElementById(selector.substr(1, selector.length - 1));
+    } else if (selector[0] == ".") {
+        var toReturn = document.getElementsByClassName(selector.substr(1, selector.length - 1));
+    } else {
+        var toReturn = document.getElementsByTagName(selector);
+    }
+    return toReturn;
+}
