c925ed3767935cd3ca4aec70ea0d7bc3db85e4da
& <pair+jackiejohnston.krnwonseungee@devbootcamp.com>
2014-04-08 16:18:54 -0700

    Moved miniquery to module object
    * Still working on AJAX since we can't really test it.
    * Looping implemented for when query returns multiple objects
    * Hooray!

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,8 +1,63 @@
 /*!
 * minQuery
 */
-// SweetSelector.select('#eyed')
-// should return <div id="eyed">eyed</div>
+var miniQuery = function(el){
+  return new miniObject(document.querySelectorAll(el))
+}
+
+var miniObject = function(el){
+  this.stuff = el
+}
+
+miniObject.prototype = {
+
+  hide: function() {
+    for (var i=0;i<this.stuff.length;i++){
+      this.stuff[i].style.display = 'none'
+    }
+  },
+  show: function() {
+    for (var i=0;i<this.stuff.length;i++){
+      this.stuff[i].style.display = ''
+    }
+  },
+
+  addClass: function(className){
+    for (var i=0;i<this.stuff.length;i++){
+      if (this.stuff[i].classList)
+        this.stuff[i].classList.add(className);
+      else
+        this.stuff[i].className += ' ' + className;
+    }
+  },
+
+  removeClass: function(className){
+    for (var i=0;i<this.stuff.length;i++){
+      if (this.stuff[i].classList)
+        this.stuff[i].classList.remove(className);
+      else
+        this.stuff[i].className = this.stuff[i].className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+  },
+
+  on: function(eventName, eventHandler){
+    for (var i=0;i<this.stuff.length;i++){
+      this.stuff[i].addEventListener(eventName, eventHandler);
+    }
+  },
+
+  trigger: function(eventName){
+    for (var i=0;i<this.stuff.length;i++){
+      var event = new Event(eventName)
+      this.stuff[i].dispatchEvent(event)
+    }
+  }
+
+
+}
+
+
+
 
 SweetSelector = {}
 
