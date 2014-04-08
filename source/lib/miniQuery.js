057965c6aea401771ede243b8040b356e79d1301
Matt Bechtel & Ryan <pair+mbech.rbn1357@devbootcamp.com>
2014-04-08 12:07:13 -0700

    Created addClass. Super DRY

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -14,16 +14,26 @@ var DOM = (function(){
        divElems[i].style.display = 'none';
       }
     },
+
     show: function(selector){
       var divElems = SweetSelector.select(selector)
       for(var i = 0; i < divElems.length; i++){
        divElems[i].style.display = '';
       }
     },
+
     removeClass: function(selector, className){
       var divElems = SweetSelector.select(selector)
       for(var i = 0; i < divElems.length; i++){
        divElems[i].classList.remove(className)
+       console.log(divElems[i])
+      }
+    },
+
+    addClass: function(selector, className){
+      var divElems = SweetSelector.select(selector)
+      for(var i = 0; i < divElems.length; i++){
+       divElems[i].classList.add(className)
        console.log(divElems[i])
       }
     }
