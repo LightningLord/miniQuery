c7657cff23f621ea7c784ed404bc3b010eaee8c7
Katherine Hayden & Jason Lo <pair+katherineimogene.lightninglord@devbootcamp.com>
2014-04-08 13:59:24 -0700

    refactored some stuff to use modules

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,64 +1,58 @@
-/*!
- * minQuery
- */
- var SweetSelector = function(){
 
- }
-
- SweetSelector.prototype = {
-  select: function(selector){
-    var selectorName = selector.substring(1)
-    if (selector[0] == '#'){
-      return document.getElementById(selectorName)
-    } else if (selector[0] == '.'){
-      return document.getElementsByClassName(selectorName)
-    } else {
-      return document.getElementsByTagName(selector)
-    }
-  }
-}
-
-
-
-var sweetSelector = new SweetSelector
-
-var DOM = function(){
-
-}
-
-DOM.prototype = {
-  domCollection: function(selector){
-    return sweetSelector.select(selector)
-  },
-
-  changeDisplay: function(cssAttribute, domCollection){
-    if (domCollection.length) {
-      for (var i = 0; i < domCollection.length; i++){
-        domCollection[i].style.display = cssAttribute
+var SweetSelector = (function(){
+  return {
+    select: function(selector){
+      var selectorName = selector.substring(1)
+      if (selector[0] == '#'){
+        return document.getElementById(selectorName)
+      } else if (selector[0] == '.'){
+        return document.getElementsByClassName(selectorName)
+      } else {
+        return document.getElementsByTagName(selector)
       }
-    } else {
-      domCollection.style.display = cssAttribute
     }
-  },
 
-  hide: function(selector){
-    this.changeDisplay('none', this.domCollection(selector))
-  },
-
-  show: function(selector){
-    this.changeDisplay('block', this.domCollection(selector))
-  },
-
-  addClass: function(selector, newClass){
-    this.domCollection(selector).classList.add(newClass)
-  },
-
-  removeClass: function(selector, badClass){
-    this.domCollection(selector).classList.remove(badClass)
   }
-}
+}) ()
 
-var dom = new DOM
+
+
+var DOM = (function(){
+  return {
+
+    domCollection: function(selector){
+      return SweetSelector.select(selector)
+    },
+
+    changeDisplay: function(cssAttribute, domCollection){
+      if (domCollection.length) {
+        for (var i = 0; i < domCollection.length; i++){
+          domCollection[i].style.display = cssAttribute
+        }
+      } else {
+        domCollection.style.display = cssAttribute
+      }
+    },
+
+    hide: function(selector){
+      this.changeDisplay('none', this.domCollection(selector))
+    },
+
+    show: function(selector){
+      this.changeDisplay('block', this.domCollection(selector))
+    },
+
+    addClass: function(selector, newClass){
+      this.domCollection(selector).classList.add(newClass)
+    },
+
+    removeClass: function(selector, badClass){
+      this.domCollection(selector).classList.remove(badClass)
+    }
+  }
+}) ()
+
+
 
 var EventDispatcher = function(){
 
@@ -104,3 +98,8 @@ AjaxWrapper.prototype = {
 }
 
 var wrapper = new AjaxWrapper
+
+
+
+
+
