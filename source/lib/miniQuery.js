1ebec7f4c55bacb9de470f3d79219bb5c9ecab2b
Matt Bechtel & Ryan <pair+mbech.rbn1357@devbootcamp.com>
2014-04-08 18:09:34 -0700

    full release minus ajax directly on $

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -23,9 +23,26 @@
       }
     }
   }
-})()
+})();
 
-var miniQuery = (function(DOM){
+var EventDispatcher = (function(){
+  return{
+    on: function(divElems, eventName, eventHandler){
+      for(var i = 0; i < divElems.length; i++){
+        divElems[i].addEventListener(eventName, eventHandler)
+      }
+    },
+    trigger: function(divElems, eventName){
+      var event = new Event(eventName)
+      for(var i = 0; i < divElems.length; i++){
+        divElems[i].dispatchEvent(event)
+      }
+    }
+  }
+})();
+
+var miniQuery = (function(DOM, EventDispatcher){
+
   var Selection = function(selector){
     this.divElems = document.querySelectorAll(selector)
   }
@@ -34,7 +51,9 @@ var miniQuery = (function(DOM){
     show: function(){DOM.show(this.divElems)},
     hide: function(){DOM.hide(this.divElems)},
     addClass: function(className){DOM.addClass(this.divElems, className)},
-    removeClass: function(className){DOM.removeClass(this.divElems, className)}
+    removeClass: function(className){DOM.removeClass(this.divElems, className)},
+    on: function(eventName, eventHandler){EventDispatcher.on(this.divElems, eventName, eventHandler)},
+    trigger: function(eventName) {EventDispatcher.trigger(this.divElems, eventName)}
   }
 
   var selectionCreator = function(selector){
@@ -42,89 +61,6 @@ var miniQuery = (function(DOM){
   }
 
   return selectionCreator
-})(DOM)
+})(DOM, EventDispatcher)
 
-
-//     DOM: {
-//       hide: function(selector){
-//         var divElems = miniQuery.SweetSelector.select(selector)
-//         for(var i = 0; i < divElems.length; i++){
-//          divElems[i].style.display = 'none';
-//         }
-//       },
-//       show: function(selector){
-//         var divElems = miniQuery.SweetSelector.select(selector)
-//         for(var i = 0; i < divElems.length; i++){
-//          divElems[i].style.display = '';
-//         }
-//       },
-//       removeClass: function(selector, className){
-//         var divElems = miniQuery.SweetSelector.select(selector)
-//         for(var i = 0; i < divElems.length; i++){
-//          divElems[i].classList.remove(className)
-//          console.log(divElems[i])
-//         }
-//       },
-
-//       addClass: function(selector, className){
-//         var divElems = miniQuery.SweetSelector.select(selector)
-//         for(var i = 0; i < divElems.length; i++){
-//          divElems[i].classList.add(className)
-//         }
-//       }
-//     },
-//     EventDispatcher: {
-//       on: function(selector, eventName, eventHandler){
-//        var divElems = miniQuery.SweetSelector.select(selector)
-//        for(var i = 0; i < divElems.length; i++){
-//          divElems[i].addEventListener(eventName, eventHandler)
-//         }
-//       },
-//       trigger: function(selector, eventName){
-//         var event = new Event(eventName)
-//         var divElems = miniQuery.SweetSelector.select(selector)
-//         for(var i = 0; i < divElems.length; i++){
-//           divElems[i].dispatchEvent(event)
-//         }
-//       }
-//     },
-//     AjaxWrapper: {
-//       request: function(reqParams){
-//         request = new XMLHttpRequest()
-//         request.open(reqParams.type, reqParams.url, true)
-//         request.onload = function(){
-//           if (request.status >= 200 && request.status < 400) {
-//             resp = request.responseText;
-//             reqParams.success(resp)
-//           }
-//           else {
-//             console.log("REQUEST FAILED")
-//             console.log(reqParams)
-//             reqParams.fail();
-//           }
-//         }
-//         request.send()
-//       }
-//     }
-//   }
-// })();
-//alias (reference to same miniQuery object)
 var $ = miniQuery
-
-
-// var miniQuery = (function(){
-//   function Thing (selector){
-//     this.opts = opts
-//   }
-//   Thing.prototype = {
-//     show: function(){
-//       console.log(this.opts)
-//     }
-//   }
-
-//   function ThingCreator(opts){
-//     return new Thing(opts);
-//   }
-
-//   return ThingCreator
-// })()
