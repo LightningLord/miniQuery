1dee1c26fd1d2c133fddf56dc63276f2b1c21aae
Matt Bechtel & Ryan <pair+mbech.rbn1357@devbootcamp.com>
2014-04-08 17:15:04 -0700

    wip - moving everything to modules

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,72 +1,112 @@
-;var miniQuery = {
-  SweetSelector: {
-      select: function(selector) {
-        return document.querySelectorAll(selector)
+;var DOM = (function(){
+  return{
+    show: function(divElems){
+      for(var i = 0; i < divElems.length; i++){
+         divElems[i].style.display = '';
       }
     },
-  DOM: {
-    hide: function(selector){
-      var divElems = miniQuery.SweetSelector.select(selector)
+    hide: function(divElems){
       for(var i = 0; i < divElems.length; i++){
        divElems[i].style.display = 'none';
       }
     },
-    show: function(selector){
-      var divElems = miniQuery.SweetSelector.select(selector)
+
+    addClass: function(divElems, className){
       for(var i = 0; i < divElems.length; i++){
-       divElems[i].style.display = '';
-      }
-    },
-    removeClass: function(selector, className){
-      var divElems = miniQuery.SweetSelector.select(selector)
-      for(var i = 0; i < divElems.length; i++){
-       divElems[i].classList.remove(className)
-       console.log(divElems[i])
+        divElems[i].classList.add(className)
       }
     },
 
-    addClass: function(selector, className){
-      var divElems = miniQuery.SweetSelector.select(selector)
+    removeClass: function(divElems, className){
       for(var i = 0; i < divElems.length; i++){
-       divElems[i].classList.add(className)
+        divElems[i].classList.remove(className)
       }
-    }
-  },
-  EventDispatcher: {
-    on: function(selector, eventName, eventHandler){
-     var divElems = miniQuery.SweetSelector.select(selector)
-     for(var i = 0; i < divElems.length; i++){
-       divElems[i].addEventListener(eventName, eventHandler)
-      }
-    },
-    trigger: function(selector, eventName){
-      var event = new Event(eventName)
-      var divElems = miniQuery.SweetSelector.select(selector)
-      for(var i = 0; i < divElems.length; i++){
-        divElems[i].dispatchEvent(event)
-      }
-    }
-  },
-  AjaxWrapper: {
-    request: function(reqParams){
-      request = new XMLHttpRequest()
-      request.open(reqParams.type, reqParams.url, true)
-      request.onload = function(){
-        if (request.status >= 200 && request.status < 400) {
-          resp = request.responseText;
-          reqParams.success(resp)
-        }
-        else {
-          console.log("REQUEST FAILED")
-          console.log(reqParams)
-          reqParams.fail();
-        }
-      }
-      request.send()
     }
   }
-}
+})()
 
+var EventDispatcher = (function(){
+  return {
+    on: function(divElems, eventName, eventHandler){
+      for(var i = 0; i < divElems.length; i++){
+         divElems[i].addEventListener(eventName, eventHandler)
+      }
+    }
+  }
+})();
+
+var miniQuery = (function(DOM){
+  var Selection = function(selector){
+    this.divElems = document.querySelectorAll(selector)
+  }
+
+  Selection.prototype = {
+    show: function(){DOM.show(this.divElems)},
+    hide: function(){DOM.hide(this.divElems)},
+    addClass: function(className){DOM.addClass(this.divElems, className)},
+    removeClass: function(className){DOM.removeClass(this.divElems, className)},
+    on: function(eventName, eventHandler){EventDispatcher.on(this.divElems, eventName, eventHandler)}
+  }
+
+  var selectionCreator = function(selector){
+    return new Selection(selector)
+  }
+
+  return selectionCreator
+})(DOM, EventDispatcher)
+
+//     EventDispatcher: {
+//       on: function(selector, eventName, eventHandler){
+//        var divElems = miniQuery.SweetSelector.select(selector)
+//        for(var i = 0; i < divElems.length; i++){
+//          divElems[i].addEventListener(eventName, eventHandler)
+//         }
+//       },
+//       trigger: function(selector, eventName){
+//         var event = new Event(eventName)
+//         var divElems = miniQuery.SweetSelector.select(selector)
+//         for(var i = 0; i < divElems.length; i++){
+//           divElems[i].dispatchEvent(event)
+//         }
+//       }
+//     },
+//     AjaxWrapper: {
+//       request: function(reqParams){
+//         request = new XMLHttpRequest()
+//         request.open(reqParams.type, reqParams.url, true)
+//         request.onload = function(){
+//           if (request.status >= 200 && request.status < 400) {
+//             resp = request.responseText;
+//             reqParams.success(resp)
+//           }
+//           else {
+//             console.log("REQUEST FAILED")
+//             console.log(reqParams)
+//             reqParams.fail();
+//           }
+//         }
+//         request.send()
+//       }
+//     }
+//   }
+// })();
 //alias (reference to same miniQuery object)
 var $ = miniQuery
 
+
+// var miniQuery = (function(){
+//   function Thing (selector){
+//     this.opts = opts
+//   }
+//   Thing.prototype = {
+//     show: function(){
+//       console.log(this.opts)
+//     }
+//   }
+
+//   function ThingCreator(opts){
+//     return new Thing(opts);
+//   }
+
+//   return ThingCreator
+// })()
