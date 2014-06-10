4f32271a8d8550dc9c0c3ecaaac8e5a4713cc259
Jason Lo <mr.lo.mhs@gmail.com>
2014-06-10 13:37:27 -0700

    14 unit tests for DOM passing

--- source/jasmine/spec/miniQuerySpec.js
+++ source/jasmine/spec/miniQuerySpec.js
@@ -19,4 +19,71 @@ describe("SweetSelector", function(){
 
 })
 
-
+describe("DOM", function(){
+  it("is defined", function(){
+    expect(DOM).toBeDefined()
+  })
+  describe("domCollection", function(){
+    it("is defined", function(){
+      expect(DOM.domCollection).toBeDefined()
+    })
+    it("can find elements using SweetSelector", function(){
+      expect(DOM.domCollection("#eyed")).toBe(SweetSelector.select("#eyed"))
+    })
+  })
+  describe("changeDisplay", function(){
+    it("is defined", function(){
+      expect(DOM.changeDisplay).toBeDefined()
+    })
+    describe("can change the display", function(){
+      it("of all elements in a collection", function(){
+        var collection = DOM.domCollection('a')
+        DOM.changeDisplay('inline-block', collection)
+        expect(collection[0].style.display).toBe('inline-block')
+      })
+      it("of a single element", function(){
+        var element = DOM.domCollection('#eyed')
+        DOM.changeDisplay('inline-block', element)
+        expect(element.style.display).toBe('inline-block')
+      })
+    })
+  })
+  describe(".hide", function(){
+    it("is defined", function(){
+      expect(DOM.hide).toBeDefined()
+    })
+    it("hides elements", function(){
+      DOM.hide('#eyed')
+      expect(DOM.domCollection('#eyed').style.display).toBe('none')
+    })
+  })
+  describe(".show", function(){
+    it("is defined", function(){
+      expect(DOM.addClass).toBeDefined()
+    })
+    it("shows an element", function(){
+      DOM.show('#eyed')
+      expect(DOM.domCollection('#eyed').style.display).toBe('block')
+    })
+  })
+  describe(".addClass", function(){
+    it("is defined", function(){
+      expect(DOM.addClass).toBeDefined()
+    })
+    it("adds a class to an element", function(){
+      DOM.addClass("#eyed", "new-class")
+      expect(DOM.domCollection("#eyed").classList[0]).toContain("new-class")
+    })
+  })
+  describe(".removeClass", function(){
+    it("is defined", function(){
+      expect(DOM.removeClass).toBeDefined()
+    })
+    it("removes a class from an element", function(){
+      DOM.addClass("#eyed", "new-class")
+      DOM.removeClass("#eyed", "new-class")
+      var result = DOM.domCollection("#eyed").classList.contains("new-class")
+      expect(result).toBeFalsy()
+    })
+  })
+})
