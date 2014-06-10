923cb3a932207ec3927027b07a2f3a770435f37c
Jason Lo <mr.lo.mhs@gmail.com>
2014-06-10 11:08:03 -0700

    implemented passing SweetSelector unit tests

--- source/jasmine/spec/miniQuerySpec.js
+++ source/jasmine/spec/miniQuerySpec.js
@@ -2,4 +2,21 @@ describe("SweetSelector", function(){
   it("is defined", function(){
     expect(SweetSelector).toBeDefined()
   })
+  it(".select is defined", function(){
+    expect(SweetSelector.select).toBeDefined()
+  })
+  describe("can find elements", function(){
+    it("by id", function(){
+      expect(SweetSelector.select('#eyed')).toBe(document.getElementById('eyed'))
+    })
+    it("by class", function(){
+      expect(SweetSelector.select('.klass')).toBe(document.getElementsByClassName('klass'))
+    })
+    it("by tag", function(){
+      expect(SweetSelector.select('a')).toBe(document.getElementsByTagName('a'))
+    })
+  })
+
 })
+
+
